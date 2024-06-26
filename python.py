3b6489056e2a2e5d841db969e2abd23e2748d7c7
srd <srd090671@gmail.com>
2024-06-26 14:02:08 +0530

    ad

---
+++ python.py
@@ -0,0 +1,209 @@
+# # mytuple = tuple(('Dave', 42, True))
+# # # print(mytuple)
+# # # print(type(mytuple))
+
+# # newlist = list(mytuple)
+# # newlist.append('Neil')
+# # newtuple = tuple(newlist)
+# # print(newtuple)
+
+
+# #NESTED DICTIONARY
+# member1 = {
+#     "name": "Plant",
+#     "instrument": "vocals"
+# }
+# member2 = {
+#     "name": "Page",
+#     "instrument": "guitar"
+# }
+# band = {
+#     "member1": member1,
+#     "member2": member2
+# }
+
+# print(band)
+# print(band["member1"]["name"])
+
+##SETS##
+
+##LOOPS##
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
